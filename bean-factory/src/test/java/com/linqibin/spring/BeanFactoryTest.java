39ffeebc2e4c6d737bdd27588e717411b80bdbb1
linqibin <1214219989@qq.com>
2023-08-19 19:58:29 +0800

    Init

---
+++ bean-factory/src/test/java/com/linqibin/spring/BeanFactoryTest.java
@@ -0,0 +1,19 @@
+package com.linqibin.spring;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 测试BeanFactory相关功能
+ *
+ * @Author linqibin
+ * @Date 2023/8/19 18:20
+ * @Email 1214219989@qq.com
+ */
+public class BeanFactoryTest {
+
+    @Test
+    public void test01() {
+
+    }
+}
+
